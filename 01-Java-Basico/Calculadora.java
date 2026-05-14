2fea70c19160b2ad6a2a333979a6c812789af549
Let <lehegar@gmail.com>
2026-05-14 17:30:39 +0200

    Calculator exercise

--- 01-Java-Basico/Calculadora.java
+++ 01-Java-Basico/Calculadora.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class Calculadora {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("Write the first number");
         int firstNumber = scanner.nextInt();
@@ -7,10 +11,18 @@ public class Calculadora {
         System.out.println("Write the second number");
         int secondNumber = scanner.nextInt();
 
-        System.out.println("Write the second number");
+        scanner.nextLine(); 
+
+        System.out.println("What's the operation?");
         String operation = scanner.nextLine();
 
-        int total;
+        int total = 0;
+
+
+        if (secondNumber == 0 && operation.equals("/") ) {
+            System.out.println("This operation isn't possible");
+            return;
+        }
 
         switch (operation) {
             case "+":
@@ -18,11 +30,25 @@ public class Calculadora {
                 break;
 
              case "-":
-                total = firstNumber + secondNumber;
+                total = firstNumber - secondNumber;
                 break;
+
+            case "*":
+                total = firstNumber * secondNumber;
+                break;
+            
+            case "/":
+                total = firstNumber / secondNumber;
+                break;
+
+            default:
+                System.out.println("Invalid operation");
+                return;
         }
 
         System.out.println("El resultado es: " + total);
+
+        scanner.close();
 
 
 }}
