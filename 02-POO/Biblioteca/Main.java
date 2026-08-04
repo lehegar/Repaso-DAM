448100b906cc34a33b6fb67e44edaf215e7cae75
Leticia Hermoso <lehegar@gmail.com>
2026-08-04 15:43:27 +0200

    Correccion en Main

--- 02-POO/Biblioteca/Main.java
+++ 02-POO/Biblioteca/Main.java
@@ -1,38 +1,29 @@
 package Biblioteca;
 
 public class Main {
-    
 
-    
-    Biblioteca biblioteca1 = new Biblioteca();
+    public static void main(String[] args) {
 
-    Libro libro1 = new Libro("1984", "George Orwell", 1949);
-    Libro libro2 = new Libro("El Quijote", "Miguel de Cervantes", 1605);
-    Libro libro3 = new Libro("Harry Potter y la piedra filosofal", "J. K. Rowling", 1997);
-    Libro libro4 = new Libro("El Señor de los Anillos", "J. R. R. Tolkien", 1954);
+        Biblioteca biblioteca = new Biblioteca();
 
+        Libro libro1 = new Libro("1984", "George Orwell", 1949);
+        Libro libro2 = new Libro("El Quijote", "Miguel de Cervantes", 1605);
+        Libro libro3 = new Libro("Harry Potter y la piedra filosofal", "J. K. Rowling", 1997);
+        Libro libro4 = new Libro("El Señor de los Anillos", "J. R. R. Tolkien", 1954);
 
-    biblioteca.anadirLibro(libro1);
-    biblioteca.anadirLibro(libro2);
-    biblioteca.anadirLibro(libro3);
-    biblioteca.anadirLibro(libro4);
+        biblioteca.anadirLibro(libro1);
+        biblioteca.anadirLibro(libro2);
+        biblioteca.anadirLibro(libro3);
+        biblioteca.anadirLibro(libro4);
 
+        biblioteca.mostrarLibros();
 
-    biblioteca.mostrarLibros();
+        biblioteca.prestarLibro("El Quijote");
 
-    biblioteca.prestarLibro("El Quijote");
+        biblioteca.mostrarLibros();
 
-    biblioteca.mostrarLibros();
+        biblioteca.devolverLibro("El Quijote");
 
-
-    biblioteca.devolverLibro("El Quijote");
-
-    biblioteca.mostrarLibros();
-
-
-
-
-
-  
-     
+        biblioteca.mostrarLibros();
+    }
 }
