7b5974da9c3ffecf96571d3cba3111ebfb06e552
Leticia <lehegar@gmail.com>
2026-08-15 19:44:20 +0200

    Actualizacion

--- 01-Java-Basico/Calculadora_Numeros.java
+++ 01-Java-Basico/Calculadora_Numeros.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Calculadora {
+public class Calculadora_Numeros {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
---
+++ 03-Colecciones y excepciones/Manejo_Excepciones.java
@@ -0,0 +1,3 @@
+public class Manejo_Excepciones {
+    
+}
